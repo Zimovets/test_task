976ec8af782b3e295380330e7a31c9d39b43b124
Zimovets Sergey <zimovets_s@ukr.net>
2018-12-19 00:44:07 +0200

    - change MainController class, change get method

--- src/main/java/zimovets/test_task/controller/MainController.java
+++ src/main/java/zimovets/test_task/controller/MainController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/main")
@@ -26,19 +30,34 @@ public class MainController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAll() {
-        Long[] a = {10L, 100L,1000L};
-        return ResponseEntity.ok(a);
-    }
+//    @GetMapping
+//    public ResponseEntity<?> getAll() {
+//        Long[] a = {10L, 100L,1000L};
+//        return ResponseEntity.ok(a);
+//    }
 
-    @GetMapping(value = "{id}")
-    public ResponseEntity<?> get(@PathVariable Long id){
-        Map<Long, Integer> a = new HashMap<>();
-        a.put(1L,10);
-        a.put(2L,100);
-        a.put(3L,1000);
-        System.out.println(a.get(id));
+//    @GetMapping(value = "{id}")
+//    public ResponseEntity<?> get(@PathVariable Long id){
+//        Map<Long, Integer> a = new HashMap<>();
+//        a.put(1L,10);
+//        a.put(2L,100);
+//        a.put(3L,1000);
+//        System.out.println(a.get(id));
+//        return ResponseEntity.ok("Hello");
+//    }
+
+//    @GetMapping
+//    public ResponseEntity<?> get(@RequestParam(name = "param", required = false) Long[] array){
+//        //System.out.println(array.toString());
+//        for (long i: array){
+//            System.out.println(i);
+//        }
+//        return ResponseEntity.ok("Hello");
+//    }
+
+    @GetMapping
+    public ResponseEntity<?> get(@RequestParam(name = "param") Optional<Long[]> array){
+        System.out.println(array.isPresent());
         return ResponseEntity.ok("Hello");
     }
 }
